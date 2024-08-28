0dc2e6d3f13a36b814fdb45a5349dafc46ae260d
ccccr <hy.ccccr@gmail.com>
2024-08-29 02:52:14 +0800

    feat: :sparkles: init and completed first version of config.h

---
+++ src/config.h
@@ -0,0 +1,12 @@
+// 0: Keyboard(up_arrow, down_arrow, left_arrow, right_arrow)
+// 1: Keyboard(w, s, a, d)
+// 2: Gamepad(PAD_UP, PAD_DOWN, PAD_LEFT, PAD_RIGHT)
+// 3: Joystick
+// TODO: 4: Joystick(serial)
+#define Mode 0 
+
+// define the pin of the joystick
+#define LeftPin 1
+#define RightPin 2
+#define UpPin 3
+#define DownPin 4
